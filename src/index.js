dc0fea7e161689358b435fccb2234e8165489ae9
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-01-24 23:51:01 -0300

    create atual date route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const fs = require('fs');
+
 const app = express()
 const port = process.env.PORT || 3000
+const date = new Date();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/:month/:day', async (req, res) => {
     let month = req.params.month;
     let day = req.params.day;
+
+    const data = require(`./db/events/${month}/${day}.json`);
+
+    res.send(data);
+})
+
+app.get('/date', async (req, res) => {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
 
     const data = require(`./db/events/${month}/${day}.json`);
 
--- src/temp.js
+++
@@ -1,3 +0,0 @@
-const data = require('./db/events/1/12.json');
-
-console.log(data.events[1].year);
