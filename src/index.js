69e65f0002af9bb1d07cf9ab5a6a1f5b9a103e0c
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-01-24 23:34:22 -0300

    create database and node route to get the data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/:month/:day', async (req, res) => {
+    let month = req.params.month;
+    let day = req.params.day;
+
+    const data = require(`./db/events/${month}/${day}.json`);
+
+    res.send(data);
+})
 
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}!`))
